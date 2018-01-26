22ce0b9c926472cfc7a49bc4e94103ac4a6847ba
李武刚 <wugang@yihe-inc.com>
2018-01-26 11:30:42 +0800

    push to qiniu

--- get_image_from_bing.py
+++ get_image_from_bing.py
@@ -5,6 +5,9 @@ import json
 import urllib
 import os
 import ssl
+from qiniu import Auth, put_file, etag, urlsafe_base64_encode
+import qiniu.config
+import datetime
 
 start_url = "https://www.bing.com/HPImageArchive.aspx?format=js&idx=0&n=1&nc=1516762817652&pid=hp&mkt=zh-CN"
 # context = ssl._create_unverified_context()
@@ -29,6 +32,36 @@ def get_url():
     except Exception as e:
         print('错误 ：', e)
 
+    return image_name
+
+
+def push_qiniu():
+    image_name = get_url()
+
+    # 七牛云的accesskey以及secretkey
+    access_key = "#####"
+    secret_key = "#####"
+
+    # 上传至七牛云的文件名
+    time = datetime.datetime.now()
+    dir = 'bing_images/{}-{}-{}/'.format(time.year, time.month, time.day)
+    key = dir + image_name
+
+    # 验证七牛云身份
+    q = Auth(access_key, secret_key)
+
+    # 七牛云的存储名字
+    bucket_name = "#####"
+
+    # 上传
+    token = q.upload_token(bucket_name, key, 3600)
+    local_image = "./images/{}".format(image_name)
+    ret, info = put_file(token, key, local_image)
+    print(info)
+    assert ret['key'] == key
+    assert ret['hash'] == etag(local_image)
+
 
 if __name__ == '__main__':
-    get_url()
+    # get_url()
+    push_qiniu()
